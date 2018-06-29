3145afbe3d134def957b180ae0d4a0fa6ed7cafb
Nate Dunn <hello@natedunn.net>
2018-06-28 21:26:27 -0500

    Test new colors and formatting

--- reboot.sh
+++ reboot.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
-# REBOOT - v.0.0.1
+############
+# Reboot
+# Author: Nate Dunn
+# URL: https://github.com/natedunn/reboot
+############
+
+# Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+Color_End='\033[0m'       # Text Reset
+
+# Welcome
+echo "${Green}Welcome to Reboot.${Color_End}"
 
 # Make sure user consents
 while true; do
-    read -p "REBOOT:SETUP will attempt to run setup scripts. Is your computer a fresh install? (Answer 'Yes' or 'No').
+    read -p "Are you ready to start? This might take a while. (Answer 'Yes' or 'No').
     > " yn
     echo
     case $yn in
@@ -15,14 +31,15 @@ done
 
 # Start fresh
 clear
-echo "Great let's continue."
+echo "Great! Let's continue."
 
 # Homebrew
+echo "${Green}→ Homebrew${Color_End}"
 which -s brew
 if [[ $? != 0 ]] ; then
     echo "Installing Homebrew."
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-    echo "Homebrew is alread installed. Updating."
+    echo "Homebrew is already installed. Updating."
     brew update
 fi
